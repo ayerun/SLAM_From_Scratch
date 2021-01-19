e887b225b37cf42608c0753db94d875047c5d8aa
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-01-19 14:24:43 -0600

    created start service cannot draw rectangle in current format without creating a new publisher every time the service is called

--- trect/src/turtle_rect.cpp
+++ trect/src/turtle_rect.cpp
@@ -5,7 +5,9 @@
 #include <std_srvs/Empty.h>
 #include <turtlesim/TeleportAbsolute.h>
 #include <turtlesim/SetPen.h>
+#include <trect/start.h>
 
+//change to static
 turtlesim::PoseConstPtr tpose;
 int max_xdot;
 int max_wdot;
@@ -22,13 +24,15 @@ void stop(ros::Publisher pub)
     pub.publish(turt_twist);
 }
 
-void turn(ros::Publisher pub, ros::Rate r)
+void turn(ros::Publisher pub)
 {
     geometry_msgs::Twist turt_twist;
-    turt_twist.angular.z = 3.14159/2;
+    turt_twist.angular.z = max_wdot;
+
+    ros::Rate r(frequency);
+
     double begin = ros::Time::now().toSec();
-    
-    while (ros::Time::now().toSec() < begin+1)
+    while (ros::Time::now().toSec() < begin+(3.14159/2/max_wdot))
     {
         pub.publish(turt_twist);
         r.sleep();
@@ -36,56 +40,103 @@ void turn(ros::Publisher pub, ros::Rate r)
     stop(pub);
 }
 
-void poseCallback(const turtlesim::PoseConstPtr& pose)
- {
-   tpose = pose;
- }
+void go(ros::Publisher pub, float dist)
+{
+    geometry_msgs::Twist turt_twist;
+    turt_twist.linear.x = max_xdot;
+
+    ros::Rate r(frequency);
+
+    double begin = ros::Time::now().toSec();
+    while (ros::Time::now().toSec() < begin+(dist/max_xdot))
+    {
+        pub.publish(turt_twist);
+        r.sleep();
+    }
+    stop(pub);
+}
+
+void poseCallback(const turtlesim::PoseConstPtr &pose)
+{
+    tpose = pose;
+}
+
+bool startCallback(trect::start::Request &req, trect::start::Response &res)
+{
+    //intialize services
+    ros::NodeHandle nh;
+    ros::ServiceClient clearClient = nh.serviceClient<std_srvs::Empty>("clear");
+    clearClient.waitForExistence();
+    ros::ServiceClient teleportClient = nh.serviceClient<turtlesim::TeleportAbsolute>("TeleportAbsolute");
+    teleportClient.waitForExistence();
+
+    //teleport to new position
+    turtlesim::TeleportAbsolute::Request tele_req;
+    turtlesim::TeleportAbsolute::Response tele_res;
+    tele_req.x = req.x;
+    tele_req.y = req.y;
+    bool success = teleportClient.call(tele_req,tele_res);
+
+    //clear background
+    std_srvs::Empty emp;
+    clearClient.call(emp);
+
+    //draw rectangle
+    // go(pub,width);
+    
+    return true;
+}
 
 int main(int argc, char** argv)
- {
-   ros::init(argc, argv, "turtle_rect");
-   ros::NodeHandle nh;
+{
+    //start node
+    ros::init(argc, argv, "turtle_rect");
+    ros::NodeHandle nh;
 
-   ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1, poseCallback);
-   ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+    //initialize subscriber and publisher
+    ros::Subscriber pose_sub = nh.subscribe("turtle1/pose", 1, poseCallback);
+    ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
 
-   ros::param::get("/max_xdot", max_xdot);
-   ros::param::get("/max_wdot", max_wdot);
-   ros::param::get("/frequency", frequency);
+    //get parameters
+    ros::param::get("/max_xdot", max_xdot);
+    ros::param::get("/max_wdot", max_wdot);
+    ros::param::get("/frequency", frequency);
+    ROS_INFO_STREAM(max_xdot);
+    ROS_INFO_STREAM(max_wdot);
+    ROS_INFO_STREAM(frequency);
 
-   ros::ServiceClient teleportClient = nh.serviceClient<turtlesim::TeleportAbsolute>("TeleportAbsolute");
-   teleportClient.waitForExistence();
-   ros::ServiceClient penClient = nh.serviceClient<turtlesim::SetPen>("SetPen");
-   penClient.waitForExistence();
-   ros::ServiceClient clearClient = nh.serviceClient<std_srvs::Empty>("clear");
-   clearClient.waitForExistence();
+    // //initialize services
+    ros::ServiceClient teleportClient = nh.serviceClient<turtlesim::TeleportAbsolute>("TeleportAbsolute");
+    teleportClient.waitForExistence();
+    ros::ServiceClient penClient = nh.serviceClient<turtlesim::SetPen>("SetPen");
+    penClient.waitForExistence();
+    ros::ServiceClient clearClient = nh.serviceClient<std_srvs::Empty>("clear");
+    clearClient.waitForExistence();
+    ros::ServiceServer service = nh.advertiseService("start", startCallback);
 
-   ROS_INFO_STREAM(max_xdot);
-   ROS_INFO_STREAM(max_wdot);
-   ROS_INFO_STREAM(frequency);
+    ros::Rate r(frequency);
+    ros::Duration(2).sleep();
 
-   ros::Rate r(frequency);
-   ros::Duration(2).sleep();
-
+    //test pen
   //  turtlesim::SetPen::Request pen_req;
   //  turtlesim::SetPen::Response pen_resp;
   //  pen_req.off = 1;
   //  bool success1 = penClient.call(pen_req,pen_resp);
 
-   turtlesim::TeleportAbsolute::Request pos;
-   turtlesim::TeleportAbsolute::Response resp;
-   pos.x = 2;
-   pos.y = 3;
-   bool success2 = teleportClient.call(pos,resp);
-   turn(pub,r);
-   
-   std_srvs::Empty empty;
-   clearClient.call(empty);
+    //test teleport
+    turtlesim::TeleportAbsolute::Request pos;
+    turtlesim::TeleportAbsolute::Response resp;
+    pos.x = 2;
+    pos.y = 3;
+    bool success2 = teleportClient.call(pos,resp);
+    turn(pub);
+    go(pub,1);
 
-//    while(ros::ok())
-//    {
-//        turn(pub,r);
-//    }
+    // while(ros::ok())
+    // {
+    //   ros::spinonce();  //process all callbacks
+    //   r.sleep();
+    // }
 
-   ros::spin();
- }
+    ros::spin();
+}
