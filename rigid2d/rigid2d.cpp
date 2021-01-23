3163f0f405da541099e2b556642fd31221e79052
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-01-22 19:53:30 -0600

    finished >> overload

--- rigid2d/rigid2d.cpp
+++ rigid2d/rigid2d.cpp
@@ -13,12 +13,47 @@ namespace rigid2d
 
         return os;
     }
+
+    std::istream & operator>>(std::istream & is, Vector2D & v)
+    {
+        is >> v.x;
+        while(is.fail())
+        {
+            is.clear();
+            is.ignore(1);
+            is >> v.x;
+        }
+        
+        is >> v.y;
+        while(is.fail())
+        {
+            is.clear();
+            is.ignore(1);
+            is >> v.y;
+        }
+        
+        return is;
+    }
+
+    Transform2D::Transform2D()
+    {
+        
+    }
 }
+//
 int main()
 {
+    //test <<
     rigid2d::Vector2D v;
     v.x = 4;
     v.y = 5;
-    std::cout<<v;
+    std::cout<<v<<std::endl;
+
+    //test >>
+    rigid2d::Vector2D myVec;
+    std::cin>>myVec;
+    std::cout<<myVec;
+
+
     return 0;
 }
