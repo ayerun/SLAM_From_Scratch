a600c479a9343b657da29a35de519dc2adf756f3
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-02-08 00:05:56 -0600

    fake_turtle and odometer read parameters

--- rigid2d/src/fake_turtle.cpp
+++ rigid2d/src/fake_turtle.cpp
@@ -17,6 +17,10 @@
 //global variables
 static ros::Publisher pub;
 static ros::Subscriber vel_sub;
+static std::string left_wheel_joint;
+static std::string right_wheel_joint;
+static double base;
+static double radius;
 
 void velCallback(const geometry_msgs::TwistConstPtr &cmd)
 {
@@ -32,6 +36,12 @@ int main(int argc, char** argv)
     //initialize publishers and subscribers
     pub = nh.advertise<sensor_msgs::JointState>("joint_state", 10);
     vel_sub = nh.subscribe("cmd_vel", 10, velCallback);
+
+    //get parameters
+    ros::param::get("/wheel_base", base);
+    ros::param::get("/wheel_radius", radius);
+    ros::param::get("/left_wheel_joint", left_wheel_joint);
+    ros::param::get("/right_wheel_joint", right_wheel_joint);
 
     return 0;
 }
--- rigid2d/src/odometer.cpp
+++ rigid2d/src/odometer.cpp
@@ -18,6 +18,12 @@
 //global variables
 static ros::Subscriber js_sub;
 static ros::Publisher pub;
+static std::string odom_frame_id;
+static std::string body_frame_id;
+static std::string left_wheel_joint;
+static std::string right_wheel_joint;
+static double base;
+static double radius;
 
 void jsCallback(const sensor_msgs::JointStateConstPtr &js)
 {
@@ -33,6 +39,14 @@ int main(int argc, char** argv)
     //initialize subscribers and publishers
     js_sub = nh.subscribe("joint_state", 10, jsCallback);
     pub = nh.advertise<nav_msgs::Odometry>("odom", 10);
+
+    //get parameters
+    ros::param::get("/odom_frame_id", odom_frame_id);
+    ros::param::get("/body_frame_id", body_frame_id);
+    ros::param::get("/left_wheel_joint", left_wheel_joint);
+    ros::param::get("/right_wheel_joint", right_wheel_joint);
+    ros::param::get("/wheel_base", base);
+    ros::param::get("/wheel_radius", radius);
 
     return 0;
 }
