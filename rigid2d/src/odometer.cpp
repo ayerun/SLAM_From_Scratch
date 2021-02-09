3bf1deb6d5721f85f1ca98c7be041b48c5e074ce
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-02-08 18:14:45 -0600

    commented odometry added tf2_ros dependency

--- rigid2d/src/odometer.cpp
+++ rigid2d/src/odometer.cpp
@@ -15,35 +15,37 @@
 #include <rigid2d/diff_drive.hpp>
 #include <sensor_msgs/JointState.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <nav_msgs/Odometry.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 //global variables
-static ros::Subscriber js_sub;
-static ros::Publisher pub;
-static std::string odom_frame_id;
-static std::string body_frame_id;
-static std::string left_wheel_joint;
-static std::string right_wheel_joint;
-static sensor_msgs::JointState js_new;
-static sensor_msgs::JointState js_old;
-static nav_msgs::Odometry odom;
-static geometry_msgs::Quaternion g_rot;
-static tf2::Quaternion rot;
-static rigid2d::Vector2D angs;
-static rigid2d::Vector2D controls;
-static rigid2d::Twist2D Vb;
-static rigid2d::DiffDrive dd;
-static double base;
-static double radius;
-static const int frequency = 200;
+static ros::Subscriber js_sub;              //joint state subscriber
+static ros::Publisher pub;                  //odometry publisher
+static tf2_ros::TransformBroadcaster br;    //transform broadcaster
+static std::string odom_frame_id;           //odometry frame name
+static std::string body_frame_id;           //body frame name
+static std::string left_wheel_joint;        //left wheel joint name
+static std::string right_wheel_joint;       //right wheel joint name
+static sensor_msgs::JointState js_new;      //incoming joint state message
+static sensor_msgs::JointState js_old;      //previous joint state message
+static nav_msgs::Odometry odom;             //odometry messgae
+static geometry_msgs::Quaternion g_rot;     //geometry messages quaternion
+static tf2::Quaternion rot;                 //tf2 quaternion
+static rigid2d::Vector2D angs;              //wheel angles
+static rigid2d::Vector2D controls;          //wheel velocities
+static rigid2d::Twist2D Vb;                 //body velocity
+static rigid2d::DiffDrive dd;               //differential drive object
+static double base;                         //wheel separation
+static double radius;                       //wheel radius
+static const int frequency = 200;           //publishing frequency
 
+/// \brief subscriber callback that tracks odometry
+/// \param js_msg - current joint state
 void jsCallback(const sensor_msgs::JointStateConstPtr &js_msg)
 {
-    using std::cout;
-    using std::endl;
-
     js_new = *js_msg;
 
     //find change in wheel angles
@@ -61,6 +63,7 @@ void jsCallback(const sensor_msgs::JointStateConstPtr &js_msg)
     js_old = js_new;
 }
 
+/// \brief oublish odometry
 void publishOdom()
 {
     odom.header.stamp = ros::Time::now();
@@ -75,6 +78,10 @@ void publishOdom()
     pub.publish(odom);
 }
 
+/// \brief initializes node, subscriber, publisher, parameters, and objects
+/// \param argc - initialization arguement
+/// \param argv - initialization arguement
+/// \return 0 at end of function
 int main(int argc, char** argv)
 {
     //start node
