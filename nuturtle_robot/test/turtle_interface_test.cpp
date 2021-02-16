34295f7425c624c9fd217b72785f3d62080e792d
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-02-16 17:24:31 -0600

    finished tests compiles cleanly

--- nuturtle_robot/test/turtle_interface_test.cpp
+++ nuturtle_robot/test/turtle_interface_test.cpp
@@ -1,105 +1,98 @@
 #include <catch_ros/catch.hpp>
 #include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
-#include <sensor_msgs/JointState.h>
 #include <geometry_msgs/Twist.h>
+#include <sensor_msgs/JointState.h>
 #include <nuturtlebot/WheelCommands.h>
 #include <nuturtlebot/SensorData.h>
 #include <rigid2d/diff_drive.hpp>
+#include <rigid2d/rigid2d.hpp>
 
-void trans_callback(const nuturtlebot::WheelCommands msg){
 
-    CHECK(msg.left_velocity == 38);
-    CHECK(msg.right_velocity == 38);
-
-    return;
+void cmdCallback(const nuturtlebot::WheelCommands msg)
+{
+    CHECK(msg.left_velocity == 256);
+    CHECK(msg.right_velocity == 256);
 }
 
 
-TEST_CASE("trans_vel","[trans_vel]") {
-    
-    // this test case subscribes to a topic and checks the results
+TEST_CASE("wheel command pure translation","[pure_translation]")
+ {
+    //setup
     ros::NodeHandle nh;
-    const ros::Subscriber sub = nh.subscribe("wheel_cmd",10,trans_callback);
+    const ros::Subscriber sub = nh.subscribe("/wheel_cmd",10,cmdCallback);
+    const ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",10,true);
 
-    // a latched publisher is used to account for the case that we publish
-    // this message before the node in turtle_interface can subscribe to the
-    // FranklinTheTurtle/cmd_vel topic
-    const ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1000,true);
+    //publish
     geometry_msgs::Twist twis;
-    twis.linear.x = 0.033;
+    twis.linear.x = 0.22;
     pub.publish(twis);
-    ros::Rate r(200);
 
-    for(int i = 0; ros::ok() && i!=200; i++){
-        ros::spinOnce();
-        r.sleep();
-    }
-
-}
-
-
-void rot_callback(const nuturtlebot::WheelCommands msg){
-
-    CHECK(msg.left_velocity == -38);
-    CHECK(msg.right_velocity == 38);
-
-    return;
-}
-
-
-TEST_CASE("rot_vel","[rot_vel]") {
-    
-    // this test case subscribes to a topic and checks the results
-    ros::NodeHandle nh;
-    const ros::Subscriber sub = nh.subscribe("wheel_cmd",10,rot_callback);
-
-    // a latched publisher is used to account for the case that we publish
-    // this message before the node in turtle_interface can subscribe to the
-    // FranklinTheTurtle/cmd_vel topic
-    const ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",1000,true);
-    geometry_msgs::Twist twis;
-    twis.angular.z = 0.4125;
-    pub.publish(twis);
-    ros::Rate r(100.0);
-
-    for(int i = 0; ros::ok() && i!=200; i++){
-        ros::spinOnce();
-        r.sleep();
-    }
-
-}
-
-
-
-void encoder_callback(const sensor_msgs::JointState::ConstPtr& msg){
-    double PI = 3.141592653589793116;
-    CHECK(msg->position[0] - PI < 0.001);
-    CHECK(msg->position[1] - PI < 0.001);
-
-    return;
-}
-
-
-TEST_CASE("encoder","[encoder]") {
-    
-    // this test case subscribes to a topic and checks the results
-    ros::NodeHandle nh;
-    const ros::Subscriber sub = nh.subscribe("joint_states",1000,encoder_callback);
-
-    // a latched publisher is used to account for the case that we publish
-    // this message before the node in turtle_interface can subscribe to the
-    // FranklinTheTurtle/cmd_vel topic
-    const ros::Publisher pub = nh.advertise<nuturtlebot::SensorData>("sensor_data",1000,true);
-    nuturtlebot::SensorData data;
-    data.left_encoder = 2048;
-    data.right_encoder = 2048;
-    pub.publish(data);
+    //spin
     ros::Rate r(100);
-
-    for(int i = 0; ros::ok() && i!=200; i++){
+    for(int i = 0; ros::ok() && i!=200; i++)
+    {
         ros::spinOnce();
         r.sleep();
     }
 
+}
+
+
+void cmdCallback2(const nuturtlebot::WheelCommands msg)
+{
+    CHECK(msg.left_velocity == -93);
+    CHECK(msg.right_velocity == 93);
+}
+
+
+TEST_CASE("wheel command pure rotation","[pure_rotation]")
+{
+    //setup
+    ros::NodeHandle nh;
+    const ros::Subscriber sub = nh.subscribe("wheel_cmd",10,cmdCallback2);
+    const ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("cmd_vel",10,true);
+
+    //publish
+    geometry_msgs::Twist twis;
+    twis.angular.z = 1;
+    pub.publish(twis);
+
+    //spin
+    ros::Rate r(100.0);
+    for(int i = 0; ros::ok() && i!=200; i++)
+    {
+        ros::spinOnce();
+        r.sleep();
+    }
+}
+
+
+
+void sensorCallback(const sensor_msgs::JointState::ConstPtr& msg)
+{
+    CHECK(rigid2d::almost_equal(msg->position[0], 2*rigid2d::PI));
+    CHECK(rigid2d::almost_equal(msg->position[1], 2*rigid2d::PI));
+}
+
+
+TEST_CASE("sensor data test","[encoder]") 
+{
+    //setup
+    ros::NodeHandle nh;
+    const ros::Subscriber sub = nh.subscribe("joint_states",10,sensorCallback);
+    const ros::Publisher pub = nh.advertise<nuturtlebot::SensorData>("sensor_data",10,true);
+
+    //publish
+    nuturtlebot::SensorData mydata;
+    mydata.left_encoder = 4096;
+    mydata.right_encoder = 4096;
+    pub.publish(mydata);
+
+    //spin
+    ros::Rate r(100);
+    for(int i = 0; ros::ok() && i!=200; i++)
+    {
+        ros::spinOnce();
+        r.sleep();
+    }
 }
